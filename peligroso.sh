a91448348a49fd45ad2c0b6940fb7a0d5ac31ab1
adnanbezerra <adnanbezerra@proton.me>
2023-04-27 14:02:27 -0300

    until and while loops

---
+++ peligroso.sh
@@ -0,0 +1,7 @@
+i=0
+
+while true
+do
+	echo "ta com medo ta? número $i"
+	(( i++ )) 
+done
---
+++ until.sh
@@ -0,0 +1,5 @@
+until [[ $order == "coffee" ]]
+do
+	echo "Would you like coffee or tea?"
+	read order
+done
